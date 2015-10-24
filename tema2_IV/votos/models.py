3ce3340241ddb4fef38d8762ed9baa0d4ac581da
Gabriel Stan <gabriel.stan.90@gmail.com>
2015-10-25 01:33:50 +0200

    modificando administracion

--- tema2_IV/votos/models.py
+++ tema2_IV/votos/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 class Empresa(models.Model):
     nombre = models.CharField(max_length=200)
-    califiacion = models.FloatField(default=0.0)
+    media_calificacion = models.FloatField(default=0.0)
 
 class Calificacion(models.Model):
-    empresa = models.ForeignKey(Empresa);
+    empresa = models.ForeignKey(Empresa)
     calificacion = models.FloatField()
