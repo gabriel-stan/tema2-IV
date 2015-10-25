e584c3d8cbc755c8450e026cc96cee2e01aae6f9
Gabriel Stan <gabriel.stan.90@gmail.com>
2015-10-25 22:05:13 +0100

    separando paquete

---
+++ django-votos/votos/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Empresa(models.Model):
+    nombre = models.CharField(max_length=200)
+    media_calificacion = models.FloatField(default=0.0)
+
+class Calificacion(models.Model):
+    empresa = models.ForeignKey(Empresa)
+    calificacion = models.FloatField()
