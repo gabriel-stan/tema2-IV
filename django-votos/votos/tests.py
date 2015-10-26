cd8ee98afdbec8c0d5298eb51c9eb5689b2e0094
Gabriel Stan <gabriel.stan.90@gmail.com>
2015-10-27 00:05:24 +0100

    testeo...

--- django-votos/votos/tests.py
+++ django-votos/votos/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
+
 # Create your tests here.
+class DummyTests(TestCase):
+    def dummy_test(self):
+        this_is_true = True
+        self.assertEqual(this_is_true, False)
+        print("dummy test passed")
+
+
+class EmpresaTest(TestCase):
+    def test(self):
+        print "empresa test"
+
